894f71b327e19ed120059690b6af7e5b4b78acf5
anh.nguyen.intern <anh.nguyen.intern@ntq-solution.com.vn>
2015-12-29 14:29:33 +0700

    hello world

--- cameraclient.py
+++ cameraclient.py
@@ -3,63 +3,31 @@ import cv2
 import numpy
 import sys
 import threading
-try:
-    import tty, termios
-except ImportError:
-    # Probably Windows.
-    try:
-        import msvcrt
-    except ImportError:
-        # FIXME what to do on other platforms?
-        # Just give up here.
-        raise ImportError('getch not available')
-    else:
-        getch = msvcrt.getch
-else:
-    def getch():
-        """getch() -> key character
 
-        Read a single keypress from stdin and return the resulting character. 
-        Nothing is echoed to the console. This call will block if a keypress 
-        is not already available, but will not wait for Enter to be pressed. 
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
 
-        If the pressed key was a modifier key, nothing will be detected; if
-        it were a special function key, it may return the first character of
-        of an escape sequence, leaving additional characters in the buffer.
-        """
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(fd)
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+def makeVideoRequest(cli, port=8002):
+    TCP_IP = cli
+    TCP_PORT = port
+    sock = socket.socket()
+    sock.connect((TCP_IP, TCP_PORT))
 
-def client(cli, port=8002):
-	TCP_IP = cli
-	TCP_PORT = port
-	sock = socket.socket()
-	capture = cv2.VideoCapture(1)
-	ret, frame = capture.read()
-	sock.connect((TCP_IP, TCP_PORT))
-	encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
-	while ret:
-	    result, imgencode = cv2.imencode('.jpg', frame, encode_param)
-	    data = numpy.array(imgencode)
-	    stringData = data.tostring()
-	    sock.send( str(len(stringData)).ljust(16));
-	    sock.send( stringData );
-	    ret, frame = capture.read()
-	    decimg=cv2.imdecode(data,1)
-	    cv2.imshow('CLIENT',decimg)
-	    # cv2.waitKey(10)
-	sock.close()
-	cv2.destroyAllWindows() 
-
-def sendRequest(title):
-	while(1):
-		print "hello world"
+    while 1:
+        length = recvall(sock,16)
+        stringData = recvall(sock, int(length))
+        data = numpy.fromstring(stringData, dtype='uint8')
+        decimg=cv2.imdecode(data,1)
+        cv2.imshow('CLIENT',decimg)
+        cv2.waitKey(10)
+    sock.close()
+    cv2.destroyAllWindows() 
 
 if __name__ == '__main__':	
-    client('127.0.0.1',)
+    makeVideoRequest('127.0.0.1',)
--- cameraserver.py
+++ cameraserver.py
@@ -1,6 +1,7 @@
 import socket
 import cv2
 import numpy
+
 def recvall(sock, count):
     buf = b''
     while count:
@@ -9,18 +10,34 @@ def recvall(sock, count):
         buf += newbuf
         count -= len(newbuf)
     return buf
-TCP_IP = ''
-TCP_PORT = 8002
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(True)
-conn, addr = s.accept()
-while 1:
-    length = recvall(conn,16)
-    stringData = recvall(conn, int(length))
-    data = numpy.fromstring(stringData, dtype='uint8')
-    decimg=cv2.imdecode(data,1)
-    cv2.imshow('SERVER',decimg)
-    cv2.waitKey(10)
-s.close()
-cv2.destroyAllWindows() 
+
+def cameraVideoGet(cli = '', port = 8002):
+    TCP_IP = cli
+    TCP_PORT = port
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((TCP_IP, TCP_PORT))
+    sock.listen(True)
+    conn, addr = sock.accept()
+
+    # camera
+    capture = cv2.VideoCapture(0)
+    ret, frame = capture.read()
+    encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
+
+    while ret:
+        result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+        data = numpy.array(imgencode)
+        stringData = data.tostring()
+        conn.send( str(len(stringData)).ljust(16));
+        conn.send( stringData );
+        ret, frame = capture.read()
+        decimg=cv2.imdecode(data,1)
+        cv2.imshow('SERVER',decimg)
+        cv2.waitKey(10)
+    s.close()
+    cv2.destroyAllWindows() 
+
+if __name__=="__main__":
+    cameraVideoGet()
+
+
